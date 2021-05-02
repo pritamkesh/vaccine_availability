d5a39a2ed29dc94b91c5294f70ff95cdbb2059ea
Pritam Kesh <pritamkesh.summercode@gmail.com>
2021-05-02 15:16:28 +0530

    Updating the streamlit app config

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from availability import get_availability
 
 st.title('Vaccine Availability')
-st.markdown('Contribute on [GitHub](https://github.com/bhavsarpratik/vaccine_availability)')
+st.markdown('Contribute on [GitHub](https://github.com/pritamkesh/vaccine_availability)')
 
 next_n_days = 3
 
@@ -15,7 +15,7 @@ for index, row in data.iterrows():
 avail_districts = list(mapper.keys())
 
 min_age_limit = st.sidebar.selectbox('Min age limit', [18, 45])
-option = st.sidebar.multiselect('How would you like to be contacted?', avail_districts, "Ahmedabad Corporation")
+option = st.sidebar.multiselect('How would you like to be contacted?', avail_districts, "East Bardhaman")
 
 district_ids = [mapper[val] for val in option]
 
